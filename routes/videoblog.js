d71ea7cc9dec4acf84923afdf7aaec073ae8904a
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-02-10 02:13:59 +0400

    Generating videoblog route

--- routes/videoblog.js
+++ routes/videoblog.js
@@ -40,7 +40,7 @@ router.get('/video/:videobloglink', function(req, res, next){
 
 router.post("/create", upload.single('image'), function(req, res, next){
   const { language, title, video_link, file_link } = req.body;
-  const generatedUrl = `${title}_${language}_${video_link}`;
+  const generatedUrl = `${title.trim()}_${language}`;
   console.log("GENERATED URL", generatedUrl);
 	if (!language || !title || !video_link || !file_link) {
     console.log("Error when getting data fields are empty")
