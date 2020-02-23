c378733a464bcf6e90cc6542e12b59f8ca48821d
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-02-23 20:23:08 +0400

    fix static data route

--- routes/singleData.js
+++ routes/singleData.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/count", function(req, res, next) {
-	const { students_count, lessons_count, teachers_count, members_count } = req.body;
+	const { students_count, lessons_count, teachers_count, members_count, supporters_count } = req.body;
 		SingleData.findOneAndUpdate({}, (err, post) => {
 			if (err) console.log(err);
 			console.log("post", post)
