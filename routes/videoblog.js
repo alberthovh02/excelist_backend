ab17f8a53a0c298920262f201a8a17e6661cbd4b
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-03-01 01:23:42 +0400

    Videoblog route fix

--- routes/videoblog.js
+++ routes/videoblog.js
@@ -71,7 +71,7 @@ router.post("/create", upload.any(), function(req, res, next){
         console.log("Error when videoblog create ", err)
 				res.json({message: "Something went wrong", code: 500})
 			}else
-			res.json({message: "Success", code: 200});
+			res.json({message: "Success", code: 200, data: post});
 		});
   }
 })
