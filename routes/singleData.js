94d8f3f3985b88d6f073411ab647632c2ab3ec5c
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-03-01 02:37:25 +0400

    singledata route fix

--- routes/singleData.js
+++ routes/singleData.js
@@ -12,7 +12,7 @@ router.get("/", function(req, res, next) {
 
 router.post("/count", function(req, res, next) {
 	const { students_count, lessons_count, teachers_count, members_count, supporters_count } = req.body;
-		SingleData.findOneAndUpdate((err, post) => {
+		SingleData.findOneAndUpdate({},(err, post) => {
 			if (err) console.log(err);
 			console.log("post", post)
 			res.json({message: "Success", code: 200, data: post});
