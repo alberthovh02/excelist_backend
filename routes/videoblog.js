cf06e069b1f4424c0a5847c54db7855c19e7db14
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-02-29 14:17:12 +0400

    Videoblog file add

--- routes/videoblog.js
+++ routes/videoblog.js
@@ -17,14 +17,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
-            cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error('Allowed only .png, .jpg, .jpeg and .gif'));
-        }
-    }
+    // fileFilter: (req, file, cb) => {
+    //     if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
+    //         cb(null, true);
+    //     } else {
+    //         cb(null, false);
+    //         return cb(new Error('Allowed only .png, .jpg, .jpeg and .gif'));
+    //     }
+    // }
 });
 
 router.get("/blogs-desc", function(req, res, next) {
