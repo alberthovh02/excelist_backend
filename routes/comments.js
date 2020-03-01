8fe96d52513110ee1a8bc9532d3d9346ef29f076
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-03-01 10:36:52 +0400

    Comments

--- routes/comments.js
+++ routes/comments.js
@@ -5,24 +5,23 @@ const Comments = require("../models/comments");
 const router = Router();
 
 router.get("/", function(req, res, next){
-  Comments.find(function(err, lesson){
+  Comments.find(function(err, comment){
     if(err) throw new Error(err);
-    res.json(lesson)
+    res.json(comment)
   })
 })
 
 router.post("/create", function(req, res, next){
-  const { title, content } = req.body;
-  const generatedUrl = `${title.trim()}`;
-  console.log("GENERATED URL", generatedUrl);
-	if (!title || !content) {
+  const { name, email, comment, parentId } = req.body;
+	if (!name || !email || !comment) {
     console.log("Error when getting data fields are empty")
 		res.json({message: "Something went wrong", code: 400})
 	} else {
 		const data = {
-			title,
-      content,
-      generatedUrl
+			name,
+      email,
+      comment,
+      parentId
 		}
 		Comments.create({...data}, (err, post) => {
 			if (err){
