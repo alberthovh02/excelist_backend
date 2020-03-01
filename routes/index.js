3e573750d3ca1f3082a4612c64a00ab96b17f013
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-03-01 15:09:55 +0400

    Lessons route fix

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const { Router } = require("express");
-// const Admin = require("../models/models/admin");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const { check, validationResult } = require('express-validator');
-// const User = require("../models/User.js");
 const Lesson = require("../models/lesson")
 const router = Router();
+
+const PATH = 'public/images/uploads/lessons';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, PATH);
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, fileName)
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Allowed only .png, .jpg, .jpeg and .gif'));
+        }
+    }
+});
 
 router.get("/", function(req, res, next){
   Lesson.find(function(err, lesson){
@@ -15,19 +34,41 @@ router.get("/", function(req, res, next){
   })
 })
 
-router.post("/create", function(req, res, next){
-  const { name, endTime, endMinutes, image } = req.body;
-  console.log(name, endTime, endMinutes, image)
-  console.log('req body>>>>>>>>>', req.body)
-  if(!name || !endTime || !endMinutes || !image){
-    res.json({message: "Empty data", code: 400})
-    next()
-  }else{
-  Lesson.create(req.body, (err, post) => {
-    if(err) throw new Error(err);
-    res.json({message: "Success", code: 200})
-  })
-}
+// router.post("/create", function(req, res, next){
+//   const { name, endTime, endMinutes, image } = req.body;
+//   console.log(name, endTime, endMinutes, image)
+//   console.log('req body>>>>>>>>>', req.body)
+//   if(!name || !endTime || !endMinutes || !image){
+//     res.json({message: "Empty data", code: 400})
+//     next()
+//   }else{
+//   Lesson.create(req.body, (err, post) => {
+//     if(err) throw new Error(err);
+//     res.json({message: "Success", code: 200})
+//   })
+// }
+// })
+
+router.post("/create", upload.single('image'), function(req, res, next){
+  const { title, content } = req.body;
+	if (!title || !content) {
+    console.log("Error when getting data fields are empty")
+		res.json({message: "Something went wrong", code: 400})
+	} else {
+		const data = {
+			title,
+			imageUrl: req.file.path,
+      content,
+      generatedUrl
+		}
+		Course.create({...data}, (err, post) => {
+			if (err){
+        console.log("Error when videoblog create ", err)
+				res.json({message: "Something went wrong", code: 500})
+			}else
+			res.json({message: "Success", code: 200, data: post});
+		});
+  }
 })
 
 router.delete("/:id", function(req, res, next){
