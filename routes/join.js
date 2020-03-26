9c6ffe0d90bc247bc8660e257f037130ab0c93d5
Albert Hovhannisyan <albert.hovhannisyan002@gmail.com>
2020-03-27 00:30:14 +0400

    Added join us route

--- routes/join.js
+++ routes/join.js
@@ -2,8 +2,31 @@ const express = require("express");
 const {Router} = require("express");
 const nodemailer = require("nodemailer");
 const router = Router();
+const multer = require('multer');
 
-router.post("/", function(req, res, next) {
+const storage = multer.diskStorage({
+    // destination: (req, file, cb) => {
+    //     cb(null, PATH);
+    // },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, fileName)
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    // fileFilter: (req, file, cb) => {
+    //     if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
+    //         cb(null, true);
+    //     } else {
+    //         cb(null, false);
+    //         return cb(new Error('Allowed only .png, .jpg, .jpeg and .gif'));
+    //     }
+    // }
+});
+
+router.post("/",upload.single('file'), function(req, res, next) {
 	const { file } = req.body;
 	console.log('File upload', file)
 
