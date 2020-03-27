e052f7064366ec85a21dacf1658239ad8ffca467
Albert Hovhannisyan <albert.hovhannisyan002@gmail.com>
2020-03-27 20:57:14 +0400

    Album delete

--- routes/albumImage.js
+++ routes/albumImage.js
@@ -71,6 +71,14 @@ router.post("/create/:id", verifyToken ,upload.single('image'),  async function(
 })
 
 
+router.delete("/:albumId/:imageId", function(req, res, next){
+  console.log(">>>>>>>>>>.", req.params)
+  Albums.findByIdAndRemove(req.params.id,(err, post) => {
+    if(err) res.json({message: "Something went wrong", code: 500});
+    else res.json({message: "Success", code: 200, data: post});
+  })
+})
+
 // router.delete("/:id", function(req, res, next){
 //   console.log(">>>>>>>>>>.", req.params)
 //   Albums.findByIdAndRemove(req.params.id,(err, post) => {
