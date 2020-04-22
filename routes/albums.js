de339d9ad698c0e5e9d93c13c8bd73ec7ec8ff8d
Albert Hovhannisyan <albert.hovhannisyan002@gmail.com>
2020-04-21 17:27:30 -0700

    album image fix

--- routes/albums.js
+++ routes/albums.js
@@ -3,39 +3,30 @@ const { Router } = require("express");
 const multer = require('multer');
 const Albums = require("../models/albums")
 const router = Router();
-var cloudinary = require('cloudinary').v2;
+// var cloudinary = require('cloudinary').v2;
 const verifyToken = require('../helpers/auth');
 const jwt = require('jsonwebtoken');
+const path = require("path")
 
-cloudinary.config({
-  cloud_name: 'dhlnheh7r',
-  api_key: '448993191284242',
-  api_secret: 'PZ-GzNd9xU6l4kirB7eKBD2F6Fw'
-});
+// cloudinary.config({
+//   cloud_name: 'dhlnheh7r',
+//   api_key: '448993191284242',
+//   api_secret: 'PZ-GzNd9xU6l4kirB7eKBD2F6Fw'
+// });
 
-const PATH = 'public/images/uploads/albums';
+const PATH = 'public/uploads/images/album';
 
 const storage = multer.diskStorage({
-    // destination: (req, file, cb) => {
-    //     cb(null, PATH);
-    // },
-    filename: (req, file, cb) => {
-        const fileName = file.originalname.toLowerCase().split(' ').join('-');
-        cb(null, fileName)
-    }
+  destination: PATH,
+  filename: (req, file, cb) => {
+      const fileName = file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+      cb(null, fileName)
+  }
 });
 
 const upload = multer({
-    storage: storage,
-    // fileFilter: (req, file, cb) => {
-    //     if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
-    //         cb(null, true);
-    //     } else {
-    //         cb(null, false);
-    //         return cb(new Error('Allowed only .png, .jpg, .jpeg and .gif'));
-    //     }
-    // }
-});
+  storage: storage,
+}).single("image");
 
 router.get("/" ,function(req, res, next){
 
@@ -45,27 +36,37 @@ router.get("/" ,function(req, res, next){
       })
   })
 
-router.post("/create", verifyToken ,upload.single('image'),  async function(req, res, next){
-
+router.post("/create", verifyToken, async function(req, res, next){
+  console.log("Body", req.body)
+  const { name } = req.body;
   jwt.verify(req.token, 'mysecretkey', async(err, authData) => {
     if(!err){
-      const { name } = req.body;
-      const resp = await cloudinary.uploader.upload(req.file.path, function(error, result){
-        if(error){
-          return error
+      upload(req, res, (err) => {
+        if(err){
+          console.log("Album image not saved");
+          res.json({message: "Album image not uploaded", code: 500})
         }
-        return result
+
       })
-      console.log('resp', resp)
+     
+      // const resp = await cloudinary.uploader.upload(req.file.path, function(error, result){
+      //   if(error){
+      //     return error
+      //   }
+      //   return result
+      // })
+      // console.log('resp', resp)
       // const generatedUrl = `${title.trim()}`;
       // console.log("GENERATED URL", generatedUrl);
     	if (!name) {
         console.log("Error when getting data fields are empty")
     		res.json({message: "Something went wrong", code: 400})
     	} else {
+       
+        const url = `/uploads/images/album/${req.file.filename}`
     		const data = {
     			name,
-    			imageUrl: resp.url,
+    			imageUrl: url,
     		}
     		Albums.create({...data}, (err, post) => {
     			if (err){
