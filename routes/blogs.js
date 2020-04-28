9f5324e851e018e303ae60763008ca6812d4dac6
Albert Hovhannisyan <albert.hovhannisyan002@gmail.com>
2020-04-28 15:37:37 -0700

    Server changed to local and blogs added in local db

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,17 +3,17 @@ const { Router } = require("express");
 const multer = require('multer');
 const Blogs = require("../models/blogs")
 const router = Router();
-var cloudinary = require('cloudinary').v2;
+// var cloudinary = require('cloudinary').v2;
 const verifyToken = require('../helpers/auth');
 const jwt = require('jsonwebtoken');
 
-cloudinary.config({
-  cloud_name: 'dhlnheh7r',
-  api_key: '448993191284242',
-  api_secret: 'PZ-GzNd9xU6l4kirB7eKBD2F6Fw'
-});
+// cloudinary.config({
+//   cloud_name: 'dhlnheh7r',
+//   api_key: '448993191284242',
+//   api_secret: 'PZ-GzNd9xU6l4kirB7eKBD2F6Fw'
+// });
 
-const PATH = 'public/images/uploads/blogs';
+const PATH = 'public/uploads/images/blogs';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -21,7 +21,6 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const fileName = file.originalname.toLowerCase().split(' ').join('-');
-        console.log('fileee', file)
         cb(null, fileName)
     }
 });
@@ -39,7 +38,6 @@ const upload = multer({
 });
 
 router.get("/" ,function(req, res, next){
-
       Blogs.find(function(err, lesson){
         if(err) throw new Error(err);
         res.json(lesson)
@@ -51,22 +49,24 @@ router.post("/create", verifyToken ,upload.single('image'),  async function(req,
   jwt.verify(req.token, 'mysecretkey', async(err, authData) => {
     if(!err){
       const { title, content } = req.body;
-      const resp = await cloudinary.uploader.upload(req.file.path, function(error, result){
-        if(error){
-          return error
-        }
-        return result
-      })
-      console.log('resp', resp)
+      // const resp = await cloudinary.uploader.upload(req.file.path, function(error, result){
+      //   if(error){
+      //     return error
+      //   }
+      //   return result
+      // })
+      // console.log('resp', resp)
       const generatedUrl = `${title.trim()}`;
-      console.log("GENERATED URL", generatedUrl);
+      // console.log("GENERATED URL", generatedUrl);
+      const url = `http://159.65.216.209:3000/uploads/images/album/${req.file.filename}`
+      // console.log("Url of blog image>>>>>>>>> ", url)
     	if (!title || !content) {
         console.log("Error when getting data fields are empty")
     		res.json({message: "Something went wrong", code: 400})
     	} else {
     		const data = {
     			title,
-    			imageUrl: resp.url,
+    			imageUrl: url,
           content,
           generatedUrl
     		}
@@ -96,7 +96,6 @@ router.put('/:id', function(req, res, next){
 })
 
 router.delete("/:id", function(req, res, next){
-  console.log(">>>>>>>>>>.", req.params)
   Blogs.findByIdAndRemove(req.params.id,(err, post) => {
     if(err) res.json({message: "Something went wrong", code: 500});
     else res.json({message: "Success", code: 200, data: post});
