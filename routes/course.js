4e4a996caf06a193750b52532ff9b3013f847b20
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-07-16 00:15:39 +0400

    Changing blog images path

--- routes/course.js
+++ routes/course.js
@@ -35,10 +35,23 @@ const upload = multer({
 });
 
 router.get("/", async function(req, res, next){
-  Course.find(function(err, lesson){
-    if(err) throw new Error(err);
-    res.json(lesson)
-  })
+  await Course.find({}, async function(err, data) {
+    const blogArr = await Course.find({})
+
+    blogArr.forEach(function(doc){
+      var hostname = doc.captionUrl.slice(33);
+      Course.update({_id: doc._id}, { $set: { imageUrl: hostname } }, (err, success) => {
+        if(!err){
+          console.log("Success")
+        }else{ 
+          console.log("Error ", err)
+        }
+      })
+    })
+
+		if (err) throw new Error(err);
+		console.log(res.json(data));
+	});
 })
 
 router.get('/:id', function(req, res, next){
